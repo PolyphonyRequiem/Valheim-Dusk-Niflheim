931e47c8abf2117033f558fd903ea0dac3f10498
Firoso <daniel@danielgreen.net>
2021-05-10 19:51:10 -0700

    addinge semantic versions

---
+++ src/Niflheim.Core.Tests/SemanticVersionTests.cs
@@ -0,0 +1,47 @@
+﻿using Niflheim.Installer;
+using Xunit;
+
+namespace Niflheim.Core.Tests
+{
+    public class SemanticVersionTests
+    {
+
+        [Theory]
+        [InlineData("1.0", "1.0.0", 0)]
+        [InlineData("1.0.0", "1.0", 0)]
+        [InlineData("1.0.0", "1.0.1", -1)]
+        [InlineData("1.0", "1.0.1", -1)]
+        [InlineData("1.0.1", "1.0", 1)]
+        [InlineData("1.0.1", "1.0.0", 1)]
+        [InlineData("1.0.0", "1.1", -1)]
+        [InlineData("1.0", "1.1.0", -1)]
+        [InlineData("1.1.0", "1.0", 1)]
+        [InlineData("1.1", "1.0.0", 1)]
+        [InlineData("10.20", "30.40", -1)]
+        [InlineData("2.1", "1.2", 1)]
+        public void TestSemanticVersionsComparisons(string x, string y, int expected)
+        {
+            var vx = SemanticVersion.Parse(x);
+            var vy = SemanticVersion.Parse(y);
+            Assert.True(vx.CompareTo(vy) == expected);
+            if (expected == 0)
+            {
+                Assert.True(vx >= vy);
+                Assert.True(vx <= vy);
+            }
+
+            if (expected == -1)
+            {
+                Assert.True(vx <= vy);
+                Assert.True(vx < vy);
+            }
+
+            if (expected == 1)
+            {
+                Assert.True(vx >= vy);
+                Assert.True(vx > vy);
+            }
+        }
+    }
+}
+
---
+++ src/Niflheim.Installer/SemanticVersion.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Niflheim.Installer
+{
+    public class SemanticVersion : IComparable<SemanticVersion>
+    {
+        private static readonly Regex versionExpression = new Regex(@"^(?<major>\d+)\.(?<minor>\d+)(\.(?<revision>[0-9]*))?$", RegexOptions.Compiled);
+
+        private SemanticVersion(int major, int minor, int revision)
+        {
+            Major = major;
+            Minor = minor;
+            Revision = revision;
+        }
+
+        public int Major { get; init; }
+        public int Minor { get; init; }
+        public int Revision { get; init; }
+
+        public static SemanticVersion Parse(string v)
+        {
+            Match matchResult = versionExpression.Match(v);
+
+            if (!matchResult.Success)
+            {
+                throw new InvalidOperationException($"The provided version string {v} was not a valid semantic version.");
+            }
+
+            if (!(matchResult.Groups.ContainsKey("major") && matchResult.Groups.ContainsKey("minor")))
+            {
+                throw new InvalidOperationException($"The provided version string {v} was not a valid semantic version. No major and minor version part were found.");
+            }
+            if (!int.TryParse(matchResult.Groups["major"].Value, out int major))
+            {
+                throw new InvalidOperationException($"The provided version string {v} was not a valid semantic version. The major part {matchResult.Groups["major"].Value} could not be parsed as an integer");
+            }
+            if (!int.TryParse(matchResult.Groups["minor"].Value, out int minor))
+            {
+                throw new InvalidOperationException($"The provided version string {v} was not a valid semantic version. The minor part {matchResult.Groups["minor"].Value} could not be parsed as an integer");
+            }
+            int revision = 0;
+            if (matchResult.Groups.ContainsKey("revision"))
+            {
+                int.TryParse(matchResult.Groups["revision"].Value, out revision);
+            }
+
+            return new SemanticVersion(major, minor, revision);
+        }
+
+        public int CompareTo(SemanticVersion y)
+        {
+            var x = this;
+            if (x.Major < y.Major)
+            {
+                return -1;
+            }
+            if (x.Major > y.Major)
+            {
+                return 1;
+            }
+
+            if (x.Minor < y.Minor)
+            {
+                return -1;
+            }
+            if (x.Minor > y.Minor)
+            {
+                return 1;
+            }
+
+            if (x.Revision < y.Revision)
+            {
+                return -1;
+            }
+            if (x.Revision > y.Revision)
+            {
+                return 1;
+            }
+
+            return 0;
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            SemanticVersion version = obj as SemanticVersion;
+            return (Major == version.Major && Minor == version.Minor && Revision == version.Revision);
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(SemanticVersion left, SemanticVersion right)
+        {
+            return (left.Equals(right));
+        }
+
+        public static bool operator !=(SemanticVersion left, SemanticVersion right)
+        {
+            return (!left.Equals(right));
+        }
+
+        public static bool operator <(SemanticVersion left, SemanticVersion right)
+        {
+            return left.CompareTo(right) == -1;
+        }
+
+        public static bool operator >(SemanticVersion left, SemanticVersion right)
+        {
+            return left.CompareTo(right) == 1;
+        }
+
+        public static bool operator <=(SemanticVersion left, SemanticVersion right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >=(SemanticVersion left, SemanticVersion right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+    }
+}
