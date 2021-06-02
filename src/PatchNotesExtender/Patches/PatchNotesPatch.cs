4f63705a61a1742a43ec959fc5d49c5a1a7eb2cc
Daniel Green <dangreen@microsoft.com>
2021-06-02 11:42:27 -0700

    adding some code for GuardianMenhir and PatchNotesExtender

---
+++ src/PatchNotesExtender/Patches/PatchNotesPatch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PatchNotesExtender.Patches
+{
+    [HarmonyPatch]
+    public static class PatchNotesPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(ChangeLog), "Start")]
+        private static void ChangeLog__Start(ref TextAsset ___m_changeLog)
+        {
+            var patchNotesUri = PatchNotesExtender.PatchNotesUri.Value;
+            if (String.IsNullOrWhiteSpace(patchNotesUri))
+            {
+                PatchNotesExtender.Log.LogWarning("patchNotesUri is null of whitespace.  No patch notes will be fetched.");
+                return;
+            }
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    var patchNotes = client.DownloadString(patchNotesUri.Trim());
+                    ___m_changeLog = new TextAsset(patchNotes + ___m_changeLog.text);
+                }
+            }
+            catch (Exception ex)
+            {
+                PatchNotesExtender.Log.LogError($"Unable to process data from '{patchNotesUri}'.  An exception occurred. {ex.ToString()}");
+            }
+        }
+    }
+}
