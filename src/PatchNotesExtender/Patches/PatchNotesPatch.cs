e28c73de61a75af70b5a4542befd84a79ca7ccfb
Firoso <daniel@danielgreen.net>
2021-07-10 18:24:16 -0700

    fixes patchnotes

--- src/PatchNotesExtender/Patches/PatchNotesPatch.cs
+++ src/PatchNotesExtender/Patches/PatchNotesPatch.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
@@ -24,9 +24,10 @@ namespace PatchNotesExtender.Patches
             }
             try
             {
-                using (HttpClient client = new HttpClient())
+                
+                using (WebClient client = new WebClient())
                 {
-                    var patchNotes = client.GetStringAsync(patchNotesUri.Trim()).Result;
+                    var patchNotes = client.DownloadString(patchNotesUri.Trim());
                     ___m_changeLog = new TextAsset(patchNotes);
                 }
             }
