9bd2694c3d56c48730f12fe616bb378091132e03
Firoso <daniel@danielgreen.net>
2021-07-25 20:52:43 -0700

    changes to default item converter and updating better trader config with mapped values.

--- src/DefaultItemsConverter/Program.cs
+++ src/DefaultItemsConverter/Program.cs
@@ -1,16 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace DefaultItemsConverter
 {
     class Program
     {
+        private record ItemData(string name, int price, int sellPrice, bool tradeable, bool sellable, bool ignorewaitfordiscovery);
         static Regex matchExpression = new Regex(@"(?<itemName>[a-zA-Z]+):\s*Stack\s*=\s*(?<stackSize>\d+?)\s*Price\s*=\s*(?<price>\d+)\s*SellPrice\s*=\s*(?<sellPrice>\d+)\sTradeable\s*=\s*(?<tradeable>true|false)\s*IgnoreWaitForDiscovery\s*=\s*(?<discovery>true|false)\s*Sellable\s*=\s*(?<sellable>true|false)");
+
+        enum SearchState
+        {
+            Find,
+            Price,
+            Sell,
+            Tradeable,
+            Sellable,
+            Ignore
+        }
+
         static void Main(string[] args)
         {
-            List<string> output = new List<string>();
+            List<ItemData> itemData = new List<ItemData>();
             foreach (var line in File.ReadAllLines(@".\vendor.cfg"))
             {
                 var result = matchExpression.Match(line);
@@ -20,16 +33,77 @@ namespace DefaultItemsConverter
                     continue;
                 }
 
-                output.Add($"[C_Items.Misc.{result.Groups["itemName"]}]");
-                output.Add($"Purchase Price = {Math.Ceiling((double)int.Parse(result.Groups["price"].Value)/int.Parse(result.Groups["stackSize"].Value))}");
-                output.Add($"Sell Price = {Math.Ceiling((double)int.Parse(result.Groups["sellPrice"].Value) / int.Parse(result.Groups["stackSize"].Value))}");
-                output.Add($"Tradeable = {bool.Parse(result.Groups["tradeable"].Value)}");
-                output.Add($"Sellable = {bool.Parse(result.Groups["sellable"].Value)}");
-                output.Add($"Ignore Trader Wait For Discovery = {bool.Parse(result.Groups["discovery"].Value)}");
-                output.Add($"");
+                itemData.Add(
+                    new ItemData(
+                        result.Groups["itemName"].Value,
+                        (int)Math.Ceiling((double)int.Parse(result.Groups["price"].Value) / int.Parse(result.Groups["stackSize"].Value)),
+                        (int)Math.Ceiling((double)int.Parse(result.Groups["sellPrice"].Value) / int.Parse(result.Groups["stackSize"].Value)),
+                        bool.Parse(result.Groups["tradeable"].Value),
+                        bool.Parse(result.Groups["sellable"].Value),
+                        bool.Parse(result.Groups["discovery"].Value)));
             }
 
-            File.WriteAllLines("out.cfg", output);
+            var configFile = File.ReadAllLines(@".\Menthus.bepinex.plugins.BetterTrader.cfg");
+
+            int i = 0;
+            SearchState state = SearchState.Find;
+            //[C_Items.Ammo.Bigfirearrow]
+            Regex headerExpression = new Regex(@"\[C_Items\.\w*\.(?<itemName>[a-zA-Z0-9_]*)\]");
+            ItemData matchingItem = default(ItemData);
+
+            while (i < configFile.Length)
+            {
+                string line = configFile[i];
+
+                switch (state)
+                {
+                    case SearchState.Find:
+                        var headerMatch = headerExpression.Match(line);
+                        if (headerMatch.Success)
+                        {
+                            var itemName = headerMatch.Groups["itemName"].Value;
+                            matchingItem = itemData.SingleOrDefault(i => i.name == itemName);
+
+                            if (matchingItem != null)
+                            {
+                                state = SearchState.Price;
+                                i += 4;
+                            }
+                        }
+                        i++;
+                        break;
+                    case SearchState.Price:
+                        configFile[i] = $"Purchase Price = {matchingItem.price}";
+                        state = SearchState.Sell;
+                        i += 5;
+                        break;
+                    case SearchState.Sell:
+                        configFile[i] = $"Sell Price = {matchingItem.sellPrice}";
+                        state = SearchState.Tradeable;
+                        i += 5;
+                        break;
+                    case SearchState.Tradeable:
+                        configFile[i] = $"Tradeable = {matchingItem.tradeable.ToString().ToLower()}";
+                        state = SearchState.Sellable;
+                        i += 5;
+                        break;
+                    case SearchState.Sellable:
+                        configFile[i] = $"Sellable = {matchingItem.sellable.ToString().ToLower()}";
+                        state = SearchState.Ignore;
+                        i += 5;
+                        break;
+                    case SearchState.Ignore:
+                        configFile[i] = $"Ignore Trader Wait For Discovery = {matchingItem.sellable.ToString().ToLower()}";
+                        state = SearchState.Find;
+                        i++;
+                        break;
+                    default:
+                        i++;
+                        break;
+                }
+            }
+
+            File.WriteAllLines("out.cfg", configFile);
         }
     }
 }
