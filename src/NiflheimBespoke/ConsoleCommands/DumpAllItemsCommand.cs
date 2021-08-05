67de9151725b831e557264ca8a9ad1c12660446e
Firoso <daniel@danielgreen.net>
2021-08-05 15:56:02 -0700

    dumpitems

---
+++ src/NiflheimBespoke/ConsoleCommands/DumpAllItemsCommand.cs
@@ -0,0 +1,38 @@
+﻿using Jotunn.Entities;
+using Jotunn.Managers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NiflheimBespoke.ConsoleCommands
+{
+    public class DumpAllItemsCommand : ConsoleCommand
+    {
+        public override string Name => "dumpitems";
+
+        public override string Help => "Writes information about all items currently registered to itemsdump.txt in the dll path for NiflheimBespoke.";
+
+        public override void Run(string[] args)
+        {
+            NiflheimBespoke.Log.LogMessage("Dumping items to itemsdump.txt");
+            List<string> outputContents = new List<string>();
+            foreach (ItemDrop.ItemData.ItemType itemType in Enum.GetValues(typeof(ItemDrop.ItemData.ItemType)))
+            {
+                var allItemsOfType = ObjectDB.instance.GetAllItems(itemType, "");
+
+                outputContents.Add($"---{Enum.GetName(typeof(ItemDrop.ItemData.ItemType), itemType)}---");
+                outputContents.Add($"");
+                foreach (var item in allItemsOfType)
+                {
+                    outputContents.Add($"{item.name}:{item.m_itemData.m_shared.m_name}");
+                }
+                outputContents.Add($"");
+            }
+
+            File.WriteAllLines("itemsdump.txt", outputContents);
+        }
+    }
+}
--- src/NiflheimBespoke/NiflheimBespoke.cs
+++ src/NiflheimBespoke/NiflheimBespoke.cs
@@ -18,8 +18,9 @@ using System.Reflection;
 using BepInEx.Logging;
 using Jotunn.Entities;
 using Jotunn.Configs;
+using NiflheimBespoke.ConsoleCommands;
 
-namespace RecipeRewriter
+namespace NiflheimBespoke
 {
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     [BepInDependency(Jotunn.Main.ModGuid)]
@@ -27,7 +28,7 @@ namespace RecipeRewriter
     {
         public const string PluginGUID = "firoso.niflheim.bespoke";
         public const string PluginName = "NiflheimBespoke";
-        public const string PluginVersion = "0.1.0";
+        public const string PluginVersion = "0.1.1";
 
         public static ManualLogSource Log { get; private set; }
 
@@ -35,9 +36,10 @@ namespace RecipeRewriter
 
         private void Awake()
         {
+            Log = this.Logger;
             this.niflheimBespokeAssets = AssetUtils.LoadAssetBundleFromResources("niflheimbespoke", Assembly.GetExecutingAssembly());
             ItemManager.OnVanillaItemsAvailable += InitializeItemsFromVanillaAvailableEvent;
-            Log = this.Logger;
+            CommandManager.Instance.AddConsoleCommand(new DumpAllItemsCommand());
         }
 
         private void InitializeItemsFromVanillaAvailableEvent()
@@ -74,115 +76,6 @@ namespace RecipeRewriter
                 Log.LogError("Sturdy Chest not registered!");
             }
         }
-
-        //private void ConfigureCoreWoodChest()
-        //{          
-        //    var coreWoodChestPrefab = PrefabManager.Instance.CreateClonedPrefab("piece_chest_corewood", "piece_chest_wood");
-        //    var coreWoodChestContainer = coreWoodChestPrefab.GetComponent<Container>();
-
-        //    // build visuals/colliders
-        //    var visualSourceChestPrefab = PrefabManager.Instance.GetPrefab("Chest");
-        //    if (visualSourceChestPrefab == null)
-        //    {
-        //        Log.LogError("visualSourceChestPrefab is null");
-        //    }
-
-        //    var visualSourceChestBase = visualSourceChestPrefab.transform.Find("Chest");
-        //    if (visualSourceChestBase == null)
-        //    {
-        //        Log.LogError("visualSourceChestBase is null");
-        //    }
-
-        //    //Filter
-        //    var chestBase = coreWoodChestPrefab.transform.Find("New/woodchest");
-        //    if (chestBase == null)
-        //    {
-        //        Log.LogError("chestbase is null");
-        //    }
-
-        //    if (chestBase.gameObject == null)
-        //    {
-        //        Log.LogError("base gameobject is null");
-        //    }
-
-        //    if (visualSourceChestBase.gameObject == null)
-        //    {
-        //        Log.LogError("visualSource gameobject is null");
-        //    }
-
-        //    if (chestBase.gameObject == null)
-        //    {
-        //        Log.LogError("base gameobject is null");
-        //    }
-
-        //    if (chestBase.gameObject.GetComponent<MeshFilter>() == null)
-        //    {
-        //        Log.LogError("base meshfilter didn't exist");
-        //    }
-
-        //    if (visualSourceChestBase.gameObject.GetComponent<MeshFilter>() == null)
-        //    {
-        //        Log.LogError("visualSource meshfilter didn't exist");
-        //    }
-
-        //    if (chestBase.gameObject.GetComponent<MeshFilter>().mesh == null)
-        //    {
-        //        Log.LogError("base meshfilter mesh member didn't exist");
-        //    }
-
-        //    if (visualSourceChestBase.gameObject.GetComponent<MeshFilter>().mesh == null)
-        //    {
-        //        Log.LogError("visualSource meshfilter mesh member didn't exist");
-        //    }
-
-        //    chestBase.gameObject.GetComponent<MeshFilter>().mesh = visualSourceChestBase.gameObject.GetComponent<MeshFilter>().mesh;
-        //    chestBase.gameObject.GetComponent<MeshFilter>().mesh = visualSourceChestBase.gameObject.GetComponent<MeshFilter>().mesh;
-
-
-        //    //Collider
-        //    var targetCollider = chestBase.gameObject.GetComponent<MeshCollider>();
-        //    var sourceCollider = visualSourceChestBase.gameObject.GetComponent<MeshCollider>();
-        //    targetCollider.sharedMesh = sourceCollider.sharedMesh;
-        //    targetCollider.sharedMaterial = sourceCollider.sharedMaterial;
-        //    targetCollider.material = sourceCollider.material;
-        //    targetCollider.cookingOptions = sourceCollider.cookingOptions;
-        //    targetCollider.convex = sourceCollider.convex;
-
-        //    //Renderer
-        //    var targetRenderer = chestBase.gameObject.GetComponent<MeshRenderer>();
-        //    var sourceRenderer = visualSourceChestBase.gameObject.GetComponent<MeshRenderer>();
-
-        //    //copy via reflection?
-        //    targetRenderer.materials = sourceRenderer.materials;
-
-        //    coreWoodChestContainer.m_width = 6;
-        //    coreWoodChestContainer.m_height = 3;
-
-        //    CustomPiece piece = new CustomPiece(coreWoodChestPrefab, new PieceConfig
-        //    {
-        //        Category = "Furniture",
-        //        CraftingStation = "piece_workbench",
-        //        Description = "A sturdy chest made from fine and core wood, and a few nails to hold it together.",
-        //        PieceTable = "Hammer",
-        //        Icon = PrefabManager.Cache.GetPrefab<Sprite>("pirate_woodwood"),
-        //        Name = "Sturdy Chest",
-        //        Requirements = new RequirementConfig[]
-        //        {
-        //            new RequirementConfig{Item = "RoundLog", Amount=5, Recover=true},
-        //            new RequirementConfig{Item = "FineWood", Amount=5, Recover=true},
-        //            new RequirementConfig{Item = "BronzeNails", Amount=8, Recover=true},
-        //        }
-        //    });
-
-        //    if (PieceManager.Instance.AddPiece(piece))
-        //    {
-        //        Log.LogMessage("Sturdy Chest registered");
-        //    }
-        //    else
-        //    {
-        //        Log.LogError("Sturdy Chest not registered!");
-        //    }
-        //}
 
         private static GameObject GetPrefabByName(string name)
         {
