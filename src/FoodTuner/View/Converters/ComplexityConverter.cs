942fae37bda90cb4055cef6e71b604dd2a8889c5
Firoso <daniel@danielgreen.net>
2021-08-15 12:11:14 -0700

    fixup relative path

--- src/FoodTuner/View/Converters/ComplexityConverter.cs
+++ src/FoodTuner/View/Converters/ComplexityConverter.cs
@@ -1,10 +1,6 @@
 ﻿using FoodTuner.Model;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace FoodTuner.View.Converters
